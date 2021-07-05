e4d5fd04aff8eb886ce594fbcf715a705026bc58
Anderson <aalvesdasilva39@gmail.com>
2021-07-05 15:48:43 -0300

    Back-end da aplicação autocenter

--- autocenter/src/main/java/br/com/autocenter/AutocenterApplication.java
+++ autocenter/src/main/java/br/com/autocenter/AutocenterApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import br.com.autocenter.controller.BrandController;
 
 @SpringBootApplication
-public class AutocenterGambaApplication {
+public class AutocenterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutocenterGambaApplication.class, args);
+		SpringApplication.run(AutocenterApplication.class, args);
 	}
 
 }
